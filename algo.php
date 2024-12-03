3029e703cc44a01a8a1f5a147e3edb9c7d718f52
SireneNouira <sirene.nouira16@gmail.com>
2024-12-03 16:59:34 +0100

    finis

---
+++ algo.php
@@ -0,0 +1,79 @@
+<?php
+
+//Exercice 1
+
+// function returnTrue(){
+//     return true;
+// }
+// var_dump(returnTrue());
+
+//Exercice 2
+
+// function returnSame($string){
+// return $string;
+// }
+// echo(returnSame("jhejfhz"));
+
+
+//Exercice 3
+
+// function addition($int, $int2){
+//     return $int + $int2;
+// }
+// echo addition(1,2);
+
+//Exercice 4
+
+// function compare($int, $int2){
+//     if($int < $int2){
+//         echo "Le premier nombre est plus petit ";
+//     } if($int > $int2){
+//          echo "Le premier nombre est plus grand";
+//     }
+//     else{
+//      echo "Les deux nombres sont identiques";
+//     }
+// }
+
+// compare(6,4);
+
+//Exercice 5
+
+// function addition($int, $string){
+//     return $int . $string;
+// }
+// echo addition(1,"sirene");
+
+
+//Exercice 6
+
+// function infoPerso($string, $string2, $int){
+//     echo "Bonjour " . $string . $string2 . " tu as " . $int;
+// }
+// infoPerso("Noouuira ", "Sirèene", 21);
+
+//Exercice 7
+
+// function infoPerso($int, $string){
+//     if($string === "homme"){
+//         if($int < 18){
+//             return "Vous êtes un homme et vous êtes mineur";
+//         }else{
+//             return "Vous êtes un homme et vous êtes majeur";
+//         }
+//     }else if($string === "femme"){
+//         if($int < 18){
+//             return "Vous êtes une femme et vous êtes mineur";
+//         }else{
+//             return "Vous êtes une femme et vous êtes majeur";
+//         }
+//     }
+// }
+// echo (infoPerso(12, "femme"));
+
+//Exercice 8
+
+// function somme($int = 1,$int2 = 2,$int3 = 3){
+//     return $int + $int2 + $int3;
+// }
+// echo somme(3, 6, 7);
